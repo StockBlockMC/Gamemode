dfe734825036d2e43624cc38da74e8aa31ddffe9
hopperelec <hopperelec@gmail.com>
2022-09-18 01:24:14 +0100

    - Added support for gamemode IDs - 'Unknown player' messages now show relevant player name - Switched 'Unknown command' for 'Too many arguments' - Made gamemode.self.x a child permission for gamemode.others.x - Fixed gamemode.others children, which used to be gamemode.self.x - Added Maven default goal

--- src/main/java/uk/co/hopperelec/mc/gamemode/GamemodePlugin.java
+++ src/main/java/uk/co/hopperelec/mc/gamemode/GamemodePlugin.java
@@ -7,70 +7,104 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.Arrays;
 import java.util.List;
 
 public final class GamemodePlugin extends JavaPlugin {
     @NotNull final List<String> gamemodes = Arrays.stream(GameMode.values()).map(Enum::name).toList();
-    
-    @NotNull private String messageForCommand(@NotNull CommandSender author, @NotNull String[] args) {
-        if (args.length == 0) {
-            if (author instanceof Player) {
-                return "You are in "+((Player) author).getGameMode()+" Mode";
-            } else {
-                return "You are in CONSOLE Mode";
+
+    @NotNull
+    private String setOwnGamemode(@NotNull CommandSender author, @NotNull GameMode gamemode) {
+        if (author instanceof Player) {
+            if (author.hasPermission("gamemode.self."+gamemode)) {
+                ((Player) author).setGameMode(gamemode);
+                return "Set own gamemode to "+gamemode+" Mode";
             }
-        } else if (args.length == 1) {
-            if (gamemodes.contains(args[0].toUpperCase())) {
-                if (author instanceof Player) {
-                    final String gamemode = args[0].toUpperCase();
-                    if (author.hasPermission("gamemode.self."+gamemode)) {
-                        ((Player) author).setGameMode(GameMode.valueOf(gamemode));
-                        return "Set own gamemode to "+gamemode+" Mode";
-                    } else {
-                        return "You don't have permission to change your own gamemode to "+gamemode+" Mode";
-                    }
-                } else {
-                    return "You can't change the console's gamemode";
-                }
-            } else {
-                if (author.hasPermission("gamemode.view")) {
-                    final Player player = Bukkit.getPlayer(args[0]);
-                    if (player != null) {
-                        return player.getDisplayName()+" is in "+player.getGameMode()+" Mode";
-                    } else {
-                        return "Unknown player";
-                    }
-                } else {
-                    return "You don't have permission to view other players' gamemode";
-                }
-            }
-        } else if (args.length == 2) {
-            final String gamemode, playername;
-            if (gamemodes.contains(args[0].toUpperCase())) {
-                gamemode = args[0].toUpperCase();
-                playername = args[1];
-            } else if (gamemodes.contains(args[1].toUpperCase())) {
-                gamemode = args[1].toUpperCase();
-                playername = args[0];
-            } else {
-                return "Unknown gamemode";
-            }
-            if (author.hasPermission("gamemode.others."+gamemode)) {
-                final Player player = Bukkit.getPlayer(playername);
-                if (player != null) {
-                    player.setGameMode(GameMode.valueOf(gamemode));
-                    return "Set "+playername+"'s gamemode to "+gamemode+" Mode";
-                } else {
-                    return "Unknown player";
-                }
-            } else {
-                return "You don't have permission to set other players' gamemode to "+gamemode+" Mode";
-            }
-        } else {
-            return "Unknown command";
+            return "You don't have permission to change your own gamemode to "+gamemode+" Mode";
         }
+        return "You can't change the console's gamemode";
+    }
+
+    @NotNull
+    private String setOthersGamemode(@NotNull CommandSender author, @NotNull Player player, @NotNull GameMode gamemode) {
+        if (author.hasPermission("gamemode.others."+gamemode)) {
+            player.setGameMode(gamemode);
+            return "Set "+player.getDisplayName()+"'s gamemode to "+gamemode+" Mode";
+        }
+        return "You don't have permission to set other players' gamemode to "+gamemode+" Mode";
+    }
+
+    @NotNull
+    private String viewOwnGamemode(@NotNull CommandSender author) {
+        if (author instanceof Player) {
+            return "You are in "+((Player) author).getGameMode()+" Mode";
+        }
+        return "You are in CONSOLE Mode";
+    }
+
+    @NotNull
+    private String viewOthersGamemode(@NotNull CommandSender author, @NotNull String playername) {
+        if (!author.hasPermission("gamemode.view")) {
+            return "You don't have permission to view other players' gamemode";
+        }
+        final Player player = Bukkit.getPlayer(playername);
+        if (player != null) {
+            return player.getDisplayName()+" is in "+player.getGameMode()+" Mode";
+        }
+        return "Unknown player '"+playername+"'";
+    }
+
+    @Nullable
+    private GameMode parseGamemode(@NotNull String potentialGamemode) {
+        if (gamemodes.contains(potentialGamemode.toUpperCase())) {
+            return GameMode.valueOf(potentialGamemode.toUpperCase());
+        }
+        final int gamemodeId;
+        try {
+            gamemodeId = Integer.parseInt(potentialGamemode);
+        } catch (NumberFormatException e) {
+            return null;
+        }
+        if (gamemodeId < gamemodes.size()) {
+            return GameMode.values()[gamemodeId];
+        }
+        return null;
+    }
+    
+    @NotNull
+    private String messageForCommand(@NotNull CommandSender author, @NotNull String[] args) {
+        if (args.length == 0) {
+            return viewOwnGamemode(author);
+        } else if (args.length == 1) {
+            final GameMode gamemode = parseGamemode(args[0]);
+            if (gamemode == null) {
+                return viewOthersGamemode(author,args[0]);
+            }
+            return setOwnGamemode(author,gamemode);
+        } else if (args.length == 2) {
+            GameMode gamemode = parseGamemode(args[0]);
+            final String playername;
+            if (gamemode == null) {
+                gamemode = parseGamemode(args[1]);
+                if (gamemode == null) {
+                    return "Unknown gamemode";
+                }
+                playername = args[1];
+            } else {
+                playername = args[0];
+            }
+            final Player player = Bukkit.getPlayer(playername);
+            if (player == null) {
+                return "Unknown player '"+playername+"'";
+            }
+            if (player == author) {
+                return setOwnGamemode(author,gamemode);
+            }
+            return setOthersGamemode(author,player,gamemode);
+        }
+        return "Too many arguments, up to 2 expected!";
     }
 
     @Override
